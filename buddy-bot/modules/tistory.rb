1a580ebd99da83f0fd2ce31925ee8b67c28c0e3f
Anh Nhân Nguyễn <anhnhan@outlook.com>
2018-06-04 21:10:27 +0200

    :F

--- buddy-bot/modules/tistory.rb
+++ buddy-bot/modules/tistory.rb
@@ -265,7 +265,7 @@ module BuddyBot::Modules::Tistory
       return nil
     end
     self.log ":ballot_box_with_check: Uploaded <#{url}> / `#{s3_filename}`: #{object.presigned_url(:get, expires_in: 604800)}", event.bot
-    result = { "id": file_id, "path": s3_filename }
+    result = { "id" => file_id, "path" => s3_filename }
     self.log ":information_desk_person: Uploaded file, `#{result}`", event.bot
     return result
   end
