da7519d01eba7f11ccc921a321ebb8d2c217b1dc
Anh Nhan Nguyen <anhnhan@outlook.com>
2018-06-09 01:29:09 +0200

    tistory: git sync command

--- buddy-bot/modules/tistory.rb
+++ buddy-bot/modules/tistory.rb
@@ -56,6 +56,12 @@ module BuddyBot::Modules::Tistory
     @@s3_bucket = @@s3.bucket(@@s3_bucket_name)
   end
 
+  # invoke this command if you want to e.g. add new audio clips or memes, but don't want to restart the bot. for now, you also have to invoke e.g. #audio-load manually afterwards.
+  pm(content: "!tistory-git-sync") do |event|
+    next unless event.user.id == 139342974776639489
+    event.channel.split_send "Done.\n#{`cd #{BuddyBot.path} && git pull && git add content/tistory-list.yml && git commit -m "tistory: update pages" && git push`}"
+  end
+
   message(start_with: /^!tistory\s/i) do |event|
     next if event.user.bot_account?
     data = event.content.scan(/^!tistory\s+<?(.*?)\s*>?\s*$/i)[0]
