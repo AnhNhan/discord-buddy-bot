489bd450055c78a0049c3af14476109cf563eda8
Anh Nhân Nguyễn <anhnhan@outlook.com>
2018-06-21 23:15:28 +0200

    crawler(tistory): better copyright notice

--- buddy-bot/modules/tistory.rb
+++ buddy-bot/modules/tistory.rb
@@ -667,7 +667,7 @@ module BuddyBot::Modules::Tistory
           return { "result" => "error" }
         end
         output_filename = scan[0][0]
-      elsif output =~ /(ERROR: This video contains content from .*?, who has blocked it on copyright grounds\.|ERROR: This video is unavailable.)/
+      elsif output =~ /(ERROR: This video contains content from .*?, who has blocked it( in your country)? on copyright grounds\.|ERROR: This video is unavailable.)/
         self.log_warning ":closed_lock_with_key: YT Video <#{url}> is blocked.", event.bot
         return { "result" => "skipped" }
       else
