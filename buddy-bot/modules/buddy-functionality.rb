7eaff51d84bf99e5237f910df0ba7cd76cb58916
Anh Nhân Nguyễn <anhnhan@outlook.com>
2018-07-18 19:07:00 +0200

    not 2m but 20m

--- buddy-bot/modules/buddy-functionality.rb
+++ buddy-bot/modules/buddy-functionality.rb
@@ -221,7 +221,7 @@ module BuddyBot::Modules::BuddyFunctionality
     event.bot.game = @@motd.sample
 
     if !@@is_crawler
-      @@scheduler.every '2m' do
+      @@scheduler.every '20m' do
         yerinpics_root = BuddyBot.path("content/yerinpics/")
         selected_file = `cd /; find #{yerinpics_root} -type f | grep -v .gitkeep | shuf -n1`
         selected_file = selected_file.sub /\n/, ""
