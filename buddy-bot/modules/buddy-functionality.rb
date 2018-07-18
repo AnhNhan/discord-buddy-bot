5ff144582ae1c97112befafb00f34ce7b583a9e4
Anh Nhân Nguyễn <anhnhan@outlook.com>
2018-07-18 18:01:39 +0200

    :F

--- buddy-bot/modules/buddy-functionality.rb
+++ buddy-bot/modules/buddy-functionality.rb
@@ -224,7 +224,7 @@ module BuddyBot::Modules::BuddyFunctionality
       @@scheduler.every '2m' do
         yerinpics_root = BuddyBot.path("content/yerinpics/")
         selected_file = `cd /; find #{yerinpics_root} -type f | grep -v .gitkeep | shuf -n1`
-        selected_file = selected_file.sub "../content", ""
+        selected_file = selected_file.sub "../content/", ""
         selected_file = selected_file.sub /\n/, ""
         self.log ":information_desk_person: `#{Time.now}` Sending `#{selected_file}` to <##{@@yerin_pic_spam_channel}>.", event.bot, Struct.new(:id).new(468731351374364672)
         event.bot.send_file @@yerin_pic_spam_channel, File.open(selected_file, "r")
